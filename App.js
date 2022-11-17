ec6de008ae1ef059c74ab75570952e195de88ed0
Jens Verswijvel <enderjeli@gmail.com>
2022-11-17 15:22:15 +0100

    fourth commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>je bolled&&é pa</Text>
+      <Text>je bolledeée&é&&é pa</Text>
       <StatusBar style="auto" />
     </View>
   );
